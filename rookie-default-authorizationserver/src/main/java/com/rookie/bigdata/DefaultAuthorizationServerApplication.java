2e4e3d0087b0a14834f6e52af6b0717acd20a146
liuxili01 <liuxili@bestseller.com.cn>
2024-06-26 13:48:39 +0800

    init

---
+++ rookie-default-authorizationserver/src/main/java/com/rookie/bigdata/DefaultAuthorizationServerApplication.java
@@ -0,0 +1,21 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname DmoApplication
+ * @Description
+ *
+ * @Author rookie
+ * @Date 2023/2/28 23:26
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class DefaultAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultAuthorizationServerApplication.class, args);
+    }
+
+}
